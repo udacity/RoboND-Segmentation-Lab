e3681af458513d7b985dec145b5433958e5292d1
Devin Anzelmo <devinanzelmo@gmail.com>
2017-09-29 08:07:48 -0700

    update write_predictions_grade_set for seglab compatibility

--- code/utils/model_tools.py
+++ code/utils/model_tools.py
@@ -1,3 +1,32 @@
+# Copyright (c) 2017, Udacity
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# The views and conclusions contained in the software and documentation are those
+# of the authors and should not be interpreted as representing official policies,
+# either expressed or implied, of the FreeBSD Project
+
+# Author: Devin Anzelmo
+
 import os
 import json
 from tensorflow.contrib.keras.python import keras 
@@ -44,8 +73,12 @@ def load_network(your_weight_filename):
         raise ValueError('No weight file found at {}'.format(weight_path))
 
 
-def write_predictions_grade_set(model, out_folder_suffix,subset_name, grading_dir_name):
-    validation_path = os.path.join('..', 'data', grading_dir_name, subset_name)
+def write_predictions_grade_set(model, out_folder_suffix,subset_name, grading_dir_name=None):
+    if grading_dir_name:
+        validation_path = os.path.join('..', 'data', grading_dir_name, subset_name)
+    else:
+        validation_path = os.path.join('..', 'data', subset_name)
+
     file_names = sorted(glob.glob(os.path.join(validation_path, 'images', '*.jpeg')))
 
     output_path = os.path.join('..', 'data', 'runs', subset_name + '_' + out_folder_suffix)
